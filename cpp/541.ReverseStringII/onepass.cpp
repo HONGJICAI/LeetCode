701336174183fc3d96e2ccc2c759838b6e05cbcd
Chua <464580613@qq.com>
2017-04-13 22:50:48 +0800

    2017.4.13

---
+++ cpp/541.ReverseStringII/onepass.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int len=s.size();
+        for(int i=0;i<len;i+=2*k)
+            if(i+k<len)
+                reverse(s,i,i+k-1);
+            else
+                reverse(s,i,len-1);
+        return s;
+    }
+    void reverse(string &s,int i,int j){
+        while(i<j){
+            char temp=s[i];
+            s[i++]=s[j];
+            s[j--]=temp;
+        }
+    }
+};
