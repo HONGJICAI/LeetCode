a97dab79e4283b7f26e1639dcf21f55c6587c179
Chua <464580613@qq.com>
2017-04-25 23:00:48 +0800

    2017.4.25

---
+++ cpp/349.IntersectionOfTwoArrays/hash.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int>map;
+        vector<int>res;
+        for(int i=0;i<nums1.size();++i)
+            map[nums1[i]]=1;
+        for(int i=0;i<nums2.size();map[nums2[i]]=0,++i)
+            if(map[nums2[i]]==1)
+                res.push_back(nums2[i]);
+        return res;
+    }
+};
