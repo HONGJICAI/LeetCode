65d7dded9a8bdb8e99cc03ff3b8ae6aa5a175c18
Chua <464580613@qq.com>
2017-10-25 22:05:22 +0800

    2017.10.25

---
+++ cpp/120.Triangle/dp.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int row=triangle.size();
+        if(row==0)
+            return 0;
+        vector<vector<int>>dp;
+        dp.push_back( triangle[0]);
+        for(int i=0;i<row-1;++i){
+            dp.push_back(vector<int>(triangle[i+1].size(),0x7fffffff));
+            for(int j=0;j<triangle[i].size();++j){
+                for(int x=0;x<=1;++x)
+                    if(j+x>=0&&j+x<triangle[i+1].size())
+                        dp[i+1][j+x]=min(dp[i+1][j+x],triangle[i+1][j+x]+dp[i][j]);
+            }
+        }
+        return *min_element(dp.back().begin(),dp.back().end());
+    }
+};
