61ed2dcb20d98b4f6571b6c519475d150782be6a
Chua <464580613@qq.com>
2017-11-26 22:29:04 +0800

    2017.11.26

---
+++ cpp/728.SelfDividingNumbers/bruteForce.cpp
@@ -0,0 +1,20 @@
+class Solution {
+    bool selfDividing(int num){
+        int remain=num;
+        while(remain){
+            int mod=remain%10;
+            if(mod==0||num%mod!=0)
+                return false;
+            remain/=10;
+        }
+        return true;
+    }
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int>res;
+        for(int i=left;i<=right;++i)
+            if(selfDividing(i))
+                res.push_back(i);
+        return res;
+    }
+};
