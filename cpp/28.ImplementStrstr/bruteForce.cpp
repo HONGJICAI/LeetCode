711443da139a92ae63cef868218545ff576774ef
Chua <464580613@qq.com>
2018-03-26 22:59:35 +0800

    2018.3.26

---
+++ cpp/28.ImplementStrstr/bruteForce.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        for(int i=0;i<=(int)haystack.size()-(int)needle.size();++i)
+            for(int j=0;;++j){
+                if(j==needle.size())
+                    return i;
+                if(needle[j]!=haystack[i+j])
+                    break;
+            }
+        return -1;
+    }
+};
