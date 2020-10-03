2c30898fa34144ea03467d9c4bb7910e25dd18d5
Chua <464580613@qq.com>
2020-10-03 22:08:33 +0800

    Update buildcpp.py

--- buildcpp.py
+++ buildcpp.py
@@ -4,11 +4,41 @@ import os
 cpps = glob.glob("cpp/**/*.cpp")
 for cpp in cpps:
     print(cpp)
-    code = ["#include <bits/stdc++.h>\nusing namespace std;\n"]
+    code = '''
+#include <bits/stdc++.h>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+struct RandomListNode {
+    int label;
+    RandomListNode *next, *random;
+    RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+};
+struct Interval {
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
+};
+struct TreeLinkNode {
+ int val;
+ TreeLinkNode *left, *right, *next;
+ TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+'''
     with open(cpp) as f:
-        code += f.readlines()
+        code += f.read()
     with open("a.cpp", "w") as f:
         f.writelines(code)
-    cmd = "g++ a.cpp -o a.so -shared -std=c++17"
+    cmd = "g++ a.cpp -o a.so -shared -fpic -std=c++17"
     if 0 != os.system(cmd):
         exit(-1)
