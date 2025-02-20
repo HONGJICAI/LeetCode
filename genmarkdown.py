bbc9da7d38b5243106695d5c676c2a80710e8916
chua <464580613@qq.com>
2025-02-20 22:56:23 +0800

    beauty readme

--- genmarkdown.py
+++ genmarkdown.py
@@ -13,6 +13,8 @@ problems.sort(key=lambda x: x['id'])
 
 print('generate markdown...')
 md = '''
+![Build](https://github.com/HONGJICAI/LeetCode/actions/workflows/ci-cpp.yml/badge.svg)
+
 # LeetCode
 
 my solution for LeetCode
@@ -23,11 +25,19 @@ my solution for LeetCode
 | ---- | ---- | ---- | ---- |
 '''
 difficultyMap = {1: 'easy', 2: 'medium', 3: 'hard'}
-solvedCpps = set(filter(lambda x: str.isdigit(x) ,[f.split('.')[0] for f in os.listdir('cpp')]))
+solvedIds = {}
+for f in os.listdir('cpp') + os.listdir('sql'):
+    id, ext = os.path.splitext(f)
+    ext = ext[1:]
+    if str.isdigit(id):
+        if int(id) not in solvedIds:
+            solvedIds[int(id)] = f'[{ext}]({ext}/{id}.{ext})'
+        else:
+            solvedIds[int(id)] += f' [{ext}]({ext}/{id}.{ext})'
 
-for problem in problems:
-    id = str(problem['id'])
-    language = f'[cpp](cpp/{id}.cpp)' if id in solvedCpps else ''
+for problem in filter(lambda x: int(x['id']) <= max(solvedIds), problems):
+    id = problem['id']
+    language = solvedIds[id] if id in solvedIds else ''
     md += f"| {id} | {problem['title']} | {language} | {difficultyMap[problem['difficulty']]} |\n"
 
 with open('README.md', 'w') as f:
